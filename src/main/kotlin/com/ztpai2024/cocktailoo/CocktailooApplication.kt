7ba4016e36fe91ab4c0b45d99bf0343b60e62f3e
kubix1605 <kubix1605@gmail.com>
2024-04-09 21:25:05 +0200

    Project Structure Update

---
+++ src/main/kotlin/com/ztpai2024/cocktailoo/CocktailooApplication.kt
@@ -0,0 +1,11 @@
+package com.ztpai2024.cocktailoo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CocktailooApplication
+
+fun main(args: Array<String>) {
+    runApplication<CocktailooApplication>(*args)
+}
---
+++ src/test/kotlin/com/ztpai2024/cocktailoo/CocktailooApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ztpai2024.cocktailoo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CocktailooApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
